8b8f3087279307d76e85968aceb68dfd501c7e1f
joel wembo <joelotepawembo@gmail.com>
2021-11-03 21:06:19 +0800

    deleta

--- AutoPush.sh
+++ AutoPush.sh
@@ -1,16 +1,12 @@
-git status
-
+echo "Enter your message"
+read message
 git add .
-
-#echo 'Enter the commit message:'
-# read commitMessage
-
-git commit -m "Interview questions update"
-
-#echo 'Enter the name of the branch:'
-#read branch
-
-#git push origin $branch
-git push origin
-
-read
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin master
+fi
