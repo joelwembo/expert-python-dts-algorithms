9c1242007a0bc91ebc8beeb59b415c81ad8fa45e
joel wembo <joelotepawembo@gmail.com>
2021-11-07 13:32:55 +0800

    Data Structure update 04/11/2021

--- data_structures/binary_tree/binary_tree_mirror.py
+++ data_structures/binary_tree/binary_tree_mirror.py
@@ -14,11 +14,11 @@ def binary_tree_mirror_dict(binary_tree_mirror_dictionary: dict, root: int):
 
 def binary_tree_mirror(binary_tree: dict, root: int = 1) -> dict:
     """
-    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 7: [8,9]}, 1)
+    >>> binary_tree_mirror({ 1: [2,3], 2: [4,8], 3: [6,7], 7: [8,9]}, 1)
     {1: [3, 2], 2: [5, 4], 3: [7, 6], 7: [9, 8]}
     >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]}, 1)
     {1: [3, 2], 2: [5, 4], 3: [7, 6], 4: [11, 10]}
-    >>> binary_tree_mirror({ 1: [2,3], 2: [4,5], 3: [6,7], 4: [10,11]}, 5)
+    >>> binary_tree_mirror({ 1: [0,3], 2: [4,5], 3: [6,7], 4: [10,11]}, 5)
     Traceback (most recent call last):
     ...
     ValueError: root 5 is not present in the binary_tree
