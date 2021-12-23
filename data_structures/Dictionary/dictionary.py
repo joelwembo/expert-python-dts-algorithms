35cd532567720836611df838ff8cf565f4305b0b
“Joel <joelotepawembo@gmail.com>
2021-12-23 13:12:07 +0800

    Data Structure update 12/11/2021

---
+++ data_structures/Dictionary/dictionary.py
@@ -0,0 +1,19 @@
+
+# create a new dictionary
+
+d = dict()
+
+# add a key - value pairs to the dictionaary
+
+
+d['xyz'] = 123
+d['abc'] = 345
+
+print (d)
+
+# print only the keys
+print (d.keys())
+
+# iterate over dictionary
+for i in d :
+    print ("%s  %d" %(i, d[i]))
