cfad1b5d09c5c13f2c07c3eae07790785d0d3615
“Joel <joelotepawembo@gmail.com>
2021-12-26 20:54:29 +0800

    Data Structure update 12/11/2021

---
+++ blockchain/web3.py
@@ -0,0 +1,17 @@
+# Block development using web3
+
+""" >>> from web3 import Web3
+
+# IPCProvider:
+>>> w3 = Web3(Web3.IPCProvider('./path/to/geth.ipc'))
+
+# HTTPProvider:
+>>> w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
+
+# WebsocketProvider:
+>>> w3 = Web3(Web3.WebsocketProvider('ws://127.0.0.1:8546'))
+
+>>> w3.isConnected()
+True """
+
+
