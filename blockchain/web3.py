9d3ec4b9546f1cca6a58d66fddbc965970755f61
“Joel <joelotepawembo@gmail.com>
2021-12-26 20:55:57 +0800

    Data Structure update 12/11/2021

--- blockchain/web3.py
+++ blockchain/web3.py
@@ -14,4 +14,9 @@
 >>> w3.isConnected()
 True """
 
+from web3 import Web3
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/<infura-project-id>'))
 
+from web3.auto.infura import w3
+
+print (w3.eth.block_number)
