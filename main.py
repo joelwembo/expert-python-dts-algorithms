5250a08cad9ad2d828b7515d257632200d1508a6
“Joel <joelotepawembo@gmail.com>
2022-02-11 17:26:25 +0800

    Data Structure update 12/11/2021

--- main.py
+++ main.py
@@ -4,18 +4,18 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import asyncio
-import time
+import datetime
 
-async def say_after(delay, what):
-    await asyncio.sleep(delay)
-    print(what)
+async def display_date():
+    loop = asyncio.get_running_loop()
+    end_time = loop.time() + 5.0
+    while True:
+        print(datetime.datetime.now())
+        if (loop.time() + 1.0 ) >= end_time:
+            break
+        await asyncio.sleep(1)
 
-async def main():
-    print(f"started at {time.strftime('%X')}")
+asyncio.run(display_date())
 
-    await say_after(1, 'hello')
-    await say_after(2, 'world')
 
-    print(f"finished at {time.strftime('%X')}")
 
-asyncio.run(main())
