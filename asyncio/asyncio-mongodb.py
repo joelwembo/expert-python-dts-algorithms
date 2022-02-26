67b599a8115520ee656fe2b902476d81570eddbb
“Joel <joelotepawembo@gmail.com>
2022-02-26 21:28:45 +0800

    update

--- asyncio/asyncio-mongodb.py
+++ asyncio/asyncio-mongodb.py
@@ -4,7 +4,7 @@ import json
 
 def mongoimport(csv_path, db_name, coll_name, db_url='localhost', db_port=27000):
     """ Imports a csv file at path csv_name to a mongo colection
-    returns: count of the documants in the new collection
+    returns: count uof the documants in the new collection
     """
     client = MongoClient(db_url, db_port)
     db = client[db_name]
