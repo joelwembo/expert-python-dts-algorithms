841d9b7e53e016508bd5d695bf7fa31057fcef23
“Joel <joelotepawembo@gmail.com>
2022-03-28 19:36:05 +0800

    cplus

---
+++ Coding-Context/23-03-2022/circle.py
@@ -0,0 +1,11 @@
+# Circle prgram
+
+from math import pi
+
+def circle_area(r):
+    if r < 0:
+        raise ValueError("The radius cannot be negative")
+    return pi*(r**2)
+
+print(circle_area(10))
+
